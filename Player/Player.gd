3e4bda7ba1e5934a8fc13a986f92fba9c3f1b8b9
☕️ <macskeptic@gmail.com>
2020-08-01 15:33:52 +0200

    player jump speed cancel

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,12 +8,12 @@ export (float) var PLAYER_FRICTION_IN_GROUND = 0.25
 export (float) var PLAYER_GRAVITY = 8 * TARGET_FPS
 export (float) var PLAYER_JUMP_FORCE = -3 * TARGET_FPS
 export (float) var PLAYER_MAX_SLOPE_ANGLE = deg2rad(46)
+export (float) var PLAYER_MAX_FALL_SPEED = -1.5 * PLAYER_JUMP_FORCE
 
 var motion = Vector2.ZERO
-
 var direction_input = Vector2.ZERO
-
 var jump_input
+var jump_input_cancel
 var physics_delta
 
 func read_direction_input():
@@ -22,26 +22,32 @@ func read_direction_input():
 
 func read_jump_input():
 	jump_input = Input.is_action_just_pressed('player_jump')
+	jump_input_cancel = Input.is_action_just_released('player_jump')
 
 func apply_player_acceleration():
 	motion.x = clamp(motion.x + (direction_input.x * PLAYER_ACCELERATION * physics_delta), -PLAYER_MAX_SPEED, PLAYER_MAX_SPEED)
 
-func apply_friction_to_motion():
-	if (direction_input.x != 0):
-		return
-	motion.x = lerp(motion.x, 0, PLAYER_FRICTION_IN_GROUND)
+func apply_friction():
+	if (direction_input.x == 0):
+		motion.x = lerp(motion.x, 0, PLAYER_FRICTION_IN_GROUND)
 
-func apply_gravity_to_motion():
-	motion.y = motion.y + (PLAYER_GRAVITY * physics_delta)
+func apply_gravity():
+	motion.y = min(motion.y + (PLAYER_GRAVITY * physics_delta), PLAYER_MAX_FALL_SPEED)
 
-func apply_jump_to_motion():
+func apply_jump():
 	if jump_input:
-		motion.y = motion.y + (PLAYER_JUMP_FORCE)
+		if is_on_floor():
+			motion.y = motion.y + (PLAYER_JUMP_FORCE)
+	else:
+		if !is_on_floor():
+			if jump_input_cancel:
+				if motion.y < PLAYER_JUMP_FORCE / 2:
+					motion.y = PLAYER_JUMP_FORCE / 2
 
 func resolve_motion():
 	motion = move_and_slide(motion, Vector2.UP, false, 4, PLAYER_MAX_SLOPE_ANGLE)
 
-func apply_direction_input_to_sprite_direction():
+func determine_sprite_direction():
 	if direction_input.x != 0:
 		$PlayerSprite.flip_h = direction_input.x < 0
 
@@ -54,10 +60,11 @@ func _physics_process(delta):
 
 	read_input()
 
+	determine_sprite_direction()
+
 	apply_player_acceleration()
-	apply_direction_input_to_sprite_direction()
-	apply_friction_to_motion()
-	apply_gravity_to_motion()
-	apply_jump_to_motion()
+	apply_friction()
+	apply_gravity()
+	apply_jump()
 
 	resolve_motion()
