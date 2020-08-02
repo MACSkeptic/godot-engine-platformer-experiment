74138d5003b78d18edb0a6d810fab99dc725c5fb
☕️ <macskeptic@gmail.com>
2020-08-02 09:36:47 +0200

    enemies explode

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -1,7 +1,11 @@
 extends KinematicBody2D
+
+const ExplosionEffect = preload('res://Effects/ExplosionEffect.tscn')
+
 export (float) var MAX_SPEED = 15
 var motion = Vector2.ZERO
 
 
 func _on_HurtBox_hit(damage) -> void:
+	Utils.instance_scene_on_main(ExplosionEffect, global_position + Vector2(4, -4))
 	queue_free()
